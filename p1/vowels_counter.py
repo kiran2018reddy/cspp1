1a7885f2197e5da212563cfd5987a1216e2ea0b2
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-02 16:39:01 -0700

    " -> 4 of 4 passed. pylint: 6.67/10 "

--- p1/vowels_counter.py
+++ p1/vowels_counter.py
@@ -5,14 +5,14 @@
 #Number of vowels: 5
 
 def main():
-	"""kk"""
+    """kk"""
     str_ = input()
-	# the input string is in s
-	# remove pass and start your code here
-	count_ = 0
+    # the input string is in s
+    # remove pass and start your code here
+    count_ = 0
     for letter_ in str_:
         if letter_ in ('a', 'e', 'i', 'o', 'u'):
             count_ += 1
     print(count_)
 if __name__== "__main__":
-	main()
+    main()
