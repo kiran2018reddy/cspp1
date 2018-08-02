e93843660d0f1b875b57adb957d9a00f4dbe3840
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-02 16:20:07 -0700

    " -> 0 of 4 passed. pylint: 0/0 "

--- p1/vowels_counter.py
+++ p1/vowels_counter.py
@@ -11,7 +11,7 @@ def main():
 	# remove pass and start your code here
 	count_ = 0
 	for letter_ in str_:
-		if letter_ in ('a', 'e', 'i', 'o', 'u')
+		if letter_ in ('a', 'e', 'i', 'o', 'u'):
 		    count_ += 1
     print(count_)
 if __name__== "__main__":
