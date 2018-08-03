5f6ff92efeb69feaaf292ce5615a9767351c9290
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-02 19:57:30 -0700

    " -> 0 of 5 passed. pylint: 0/0 "

--- p3/longest_substring.py
+++ p3/longest_substring.py
@@ -1,6 +1,6 @@
 '''largest'''
 def main():
-	"""gives largest substring"""
+    """gives largest substring"""
     str_ = input()
     tstr_ = ""
     for i, item in enumerate(str_):
@@ -12,10 +12,10 @@ def main():
             if ord(tt_) <= ord(str_[ji_]):
                 tt_ = str_[ji_]
             else:
-                flag_ = 1
+            flag_ = 1
             ji_ += 1
         if len(tstr2_) > len(tstr_):
             tstr_ = tstr2_
     print(tstr_)
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
