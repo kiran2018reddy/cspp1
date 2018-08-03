d9cb5bc9412e04f0734dc3f24919fbf8a2971128
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-02 18:38:22 -0700

    " -> 5 of 5 passed. pylint: -2.00/10 "

--- p2/bob_counter.py
+++ p2/bob_counter.py
@@ -2,14 +2,14 @@
 Number of times bob occurs is: 2'''
 def main():
 	"""string"""
-	s_= input()
+	str1_= input()
 	# the input string is in s
 	# remove pass and start your code here
 	st_ ='bob'
-	c_ = 0
-	for i in range(len(s_)-2):
-		if st_ == s_[i]+s_[i+1]+s_[i+2]:
-			c_ = c_ + 1
-	print(c_)
+	cou_ = 0
+	for i in range(len(str1_)-2):
+		if st_ == str1_[i]+str1_[i+1]+str1_[i+2]:
+			cou_ = cou_ + 1
+	print(cou_)
 if __name__== "__main__":
 	main()
