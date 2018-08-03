ab122a4896b554073988993748cf1510509fb2d9
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-02 18:39:45 -0700

    " -> 5 of 5 passed. pylint: 6.00/10 "

--- p2/bob_counter.py
+++ p2/bob_counter.py
@@ -1,15 +1,15 @@
 '''Assume s is a string of lower case chara
 Number of times bob occurs is: 2'''
 def main():
-	"""string"""
-	str1_= input()
-	# the input string is in s
-	# remove pass and start your code here
-	st_ ='bob'
-	cou_ = 0
-	for i in range(len(str1_)-2):
-		if st_ == str1_[i]+str1_[i+1]+str1_[i+2]:
-			cou_ = cou_ + 1
-	print(cou_)
+    """string"""
+    str1_= input()
+    # the input string is in s
+    # remove pass and start your code here
+    st_ ='bob'
+    cou_ = 0
+    for i in range(len(str1_)-2):
+        if st_ == str1_[i]+str1_[i+1]+str1_[i+2]:
+            cou_ = cou_ + 1
+    print(cou_)
 if __name__== "__main__":
 	main()
