6d265532ab6acac48b8fd394b0a25842bbb2bc23
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-02 18:36:03 -0700

    " -> 0 of 5 passed. pylint: -6.00/10 "

--- p2/bob_counter.py
+++ p2/bob_counter.py
@@ -5,11 +5,11 @@ def main():
 	s_= input()
 	# the input string is in s
 	# remove pass and start your code here
-	st_='bob'
-	c = 0
+	st_ ='bob'
+	c_ = 0
 	for i in range(len(s_)-2):
 		if s_ == s_[i]+s_[i+1]+s_[i+2]:
-			c = c + 1
-	print(c)
+			c_ = c_ + 1
+	print(c_)
 if __name__== "__main__":
 	main()
